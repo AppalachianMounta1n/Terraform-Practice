67415059408f2482bdc44d42b09a47be88e0c0a6
Lily Gross <lilybethgross@gmail.com>
2026-03-04 11:41:48 -0800

    GCP Variables Configured

---
+++ GCP/Variables/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance"
+    machine_type = "f1-micro"
+    tags = ["web", "dev"]
+
+    boot_disk {
+        initialize_params {
+            image = "cos-cloud/cos-stable"
+        }
+    }
+
+    network_interface {
+        network = google_compute_network.vpc_network.name
+        access_config {
+
+        }
+    }
+}
+
+resource "google_compute_firewall" "firewall_rules" {
+    name = "basic-firewall"
+    network = google_compute_network.vpc_network.name
+
+    allow {
+        protocol = "icmp"
+    }
+
+    allow {
+        protocol = "tcp"
+        ports = ["80", "8080", "1000-2000"]
+    }
+
+    source_tags = ["web"]
+}
---
+++ GCP/Variables/variables.tf
@@ -0,0 +1,11 @@
+variable "project" { 
+    default = "learn-terraform-for-gcp-489218"
+}
+
+variable "region" {
+    default = "us-west1"
+}
+
+variable "zone" {
+    default = "us-west1-a"
+}
