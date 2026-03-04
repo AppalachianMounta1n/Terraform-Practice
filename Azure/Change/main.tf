acf78f4fbffa9de294bb7698f41e7d16cc1aa8e4
Lily Gross <lilybethgross@gmail.com>
2026-03-04 12:49:13 -0800

    Azure Change Formatting

--- Azure/Change/main.tf
+++ Azure/Change/main.tf
@@ -19,8 +19,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    name = "AzureVnet"
-    address_space = ["10.0.0.0/16"]
-    location = "westus2"
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = "AzureVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
 }
