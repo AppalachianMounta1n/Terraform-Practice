f52d7ccdfd941fdc34bd70b7b972ad42a9d0489b
Lily Gross <lilybethgross@gmail.com>
2026-03-04 12:22:44 -0800

    OCI Outputs Configured

--- Oracle Cloud/Outputs/main.tf
+++ Oracle Cloud/Outputs/main.tf
@@ -7,9 +7,9 @@ terraform {
 }
 
 provider "oci" {
-  region              = "us-sanjose-1"
-  auth                = "SecurityToken"
-  config_file_profile = "learn-terraform"
+  region              = var.region
+  auth                = var.auth_type
+  config_file_profile = var.config_file_profile
 }
 
 resource "oci_core_vcn" "internal" {
@@ -17,4 +17,13 @@ resource "oci_core_vcn" "internal" {
   cidr_block     = "172.16.0.0/20"
   compartment_id = var.compartment_id
   display_name   = "Internal VCN"
+}
+
+resource "oci_core_subnet" "dev" {
+  vcn_id                     = oci_core_vcn.internal.id
+  cidr_block                 = "172.16.0.0/24"
+  compartment_id             = var.compartment_id
+  display_name               = "Dev Subnet"
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "dev"
 }
