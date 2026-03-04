2e0ad6c72399572a6e15d9411f8554c9ba56b210
Lily Gross <lilybethgross@gmail.com>
2026-03-04 12:16:39 -0800

    Destructive change to subnet name

--- Oracle Cloud/Change/main.tf
+++ Oracle Cloud/Change/main.tf
@@ -23,7 +23,7 @@ resource "oci_core_subnet" "dev" {
   vcn_id                     = oci_core_vcn.internal.id
   cidr_block                 = "172.16.0.0/24"
   compartment_id             = var.compartment_id
-  display_name               = "Dev Subnet 1"
+  display_name               = "Dev Subnet"
   prohibit_public_ip_on_vnic = true
   dns_label                  = "dev"
 }
