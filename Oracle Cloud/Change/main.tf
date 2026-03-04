43d842969119f8eb5408ddc3caa8771d3dc67e1f
Lily Gross <lilybethgross@gmail.com>
2026-03-04 12:16:19 -0800

    Prep OCI Formatting, Configure OCI Change section

--- Oracle Cloud/Change/main.tf
+++ Oracle Cloud/Change/main.tf
@@ -18,3 +18,12 @@ resource "oci_core_vcn" "internal" {
   compartment_id = var.compartment_id
   display_name   = "Internal VCN"
 }
+
+resource "oci_core_subnet" "dev" {
+  vcn_id                     = oci_core_vcn.internal.id
+  cidr_block                 = "172.16.0.0/24"
+  compartment_id             = var.compartment_id
+  display_name               = "Dev Subnet 1"
+  prohibit_public_ip_on_vnic = true
+  dns_label                  = "dev"
+}
